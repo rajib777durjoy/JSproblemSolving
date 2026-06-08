218359c8507c3ff60b519602c7079d2ff8ca8112
rajib777durjoy <rajib777chanda@gmail.com>
2026-06-09 01:53:15 +0600

    solve EvenAndOddNumber

---
+++ 2.problem.js
@@ -0,0 +1,13 @@
+
+function EvenAndOddNum(num){
+const number = Number(num)
+if( number % 2 === 0){
+  return 'Even Number'
+}
+else{
+    return "Odd Number"
+}
+
+}
+const result =EvenAndOddNum(7);
+console.log(result)
